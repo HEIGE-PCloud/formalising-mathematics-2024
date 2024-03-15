18e22c513b15d1083266f758c3483081673f051c
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-03-15 10:28:18 +0000

    Section 1 Sheet 5

--- FormalisingMathematics2024/Section01logic/Sheet5.lean
+++ FormalisingMathematics2024/Section01logic/Sheet5.lean
@@ -16,7 +16,7 @@ We learn about how to manipulate `P ↔ Q` in Lean.
 You'll need to know about the tactics from the previous sheets,
 and also the following two new tactics:
 
-* `refl`
+* `rfl`
 * `rw`
 
 -/
@@ -25,41 +25,72 @@ and also the following two new tactics:
 variable (P Q R S : Prop)
 
 example : P ↔ P := by
-  sorry
+  rfl
   done
 
 example : (P ↔ Q) → (Q ↔ P) := by
-  sorry
+  intro h
+  rw [h]
   done
 
 example : (P ↔ Q) ↔ (Q ↔ P) := by
-  sorry
+  constructor
+  intro h
+  rw [h]
+  intro h
+  rw [h]
   done
 
 example : (P ↔ Q) → (Q ↔ R) → (P ↔ R) := by
-  sorry
+  intro hpq
+  intro hqr
+  rw [hpq]
+  assumption
   done
 
 example : P ∧ Q ↔ Q ∧ P := by
-  sorry
+  constructor <;>
+  · rintro ⟨h1, h2⟩
+    exact ⟨h2, h1⟩
   done
 
 example : (P ∧ Q) ∧ R ↔ P ∧ Q ∧ R := by
-  sorry
+  constructor
+  . rintro ⟨⟨hp, hq⟩, hr⟩
+    exact ⟨hp, hq, hr⟩
+  · rintro ⟨hp, hq, hr⟩
+    exact ⟨⟨hp, hq⟩, hr⟩
   done
 
 example : P ↔ P ∧ True := by
-  sorry
+  constructor
+  . intro hp
+    constructor
+    . assumption
+    . triv
+  . intro ⟨hp, _⟩
+    assumption
   done
 
 example : False ↔ P ∧ False := by
-  sorry
+  constructor
+  · rintro ⟨⟩
+  · rintro ⟨-, ⟨⟩⟩
   done
 
 example : (P ↔ Q) → (R ↔ S) → (P ∧ R ↔ Q ∧ S) := by
-  sorry
+  intro hpq
+  intro hrs
+  rw [hpq, hrs]
   done
 
 example : ¬(P ↔ ¬P) := by
-  sorry
+  intro h
+  cases' h with h1 h2
+  by_cases hP : P
+  . apply h1 <;>
+    assumption
+  . apply h1 <;>
+    apply h2 <;>
+    assumption
   done
